fcd4ec435e5f32166ce6b50445f2c314063ca348
ArturMugin <arturjancik@gmail.com>
2023-06-07 20:41:58 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string mainStr = "{([";
+        string secondStr = "})]";
+        string s = "{(}))";
+
+        for (int i = 0; i <= s.Length; i++)
+        {
+            if (mainStr.Contains(s[0]) && s.Contains(secondStr[mainStr.IndexOf(s[0])]))
+            {
+                s = s.Remove(s.IndexOf(secondStr[mainStr.IndexOf(s[0])]), 1);
+                s = s.Remove(0, 1);
+            }
+        }
+        bool result = s.Length == 0 ? true : false;
+        Console.WriteLine(result);
+        //return result;
+    }
+}
